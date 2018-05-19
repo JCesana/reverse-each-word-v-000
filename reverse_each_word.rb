e316626e4ec71cd8096c45cff81c277c8360aa8f
Josh Cesana <joshuacesana@gmail.com>
2018-05-19 19:28:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,18 @@ def reverse_each_word(sentence)
   reversedArray = sentenceArray.collect { |word| word.reverse } 
   reversedArray.join(" ")
 end 
+
+
+
+
+# solution:
+
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
